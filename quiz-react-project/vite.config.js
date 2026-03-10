b5855007e28dc106bb2113820d9279c5da60aa49
Balen <105431372+Bindekere@users.noreply.github.com>
2026-03-10 18:00:10 +0300

    Fix base URL for Netlify deployment

--- quiz-react-project/vite.config.js
+++ quiz-react-project/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/scrimba-solo-quiz-project/",
+  base: "/",
   plugins: [react()]
 })
